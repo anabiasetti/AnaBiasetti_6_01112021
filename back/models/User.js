ddd493f1174ff7bbfd6eafeb5fb37addb64e1095
Ana Biasetti <anabiasetti@protonmail.com>
2021-11-09 15:32:06 +0100

    Logique de signup

--- back/models/User.js
+++ back/models/User.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
  */
 const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
-  email: { type: string, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 /**
@@ -17,4 +17,4 @@ const userSchema = mongoose.Schema({
  */
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose("User");
+module.exports = mongoose.model("User", userSchema);
