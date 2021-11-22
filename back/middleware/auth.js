26e3344baba86a839618294d30a42792aea731e8
Ana Biasetti <anabiasetti@protonmail.com>
2021-11-22 13:34:43 +0100

    Ajout de control d'authentification pour DELETE et PUT(afin d'empêcher la modification ou la suppression d'une sauce par une autre personne que l'auteur de la sauce)

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const Sauce = require("../models/Sauce");
 require("dotenv").config();
 
 module.exports = (req, res, next) => {
@@ -7,7 +8,22 @@ module.exports = (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
+      // Si userId existe, vérifier par rapport au userId dans le token
       throw "Invalid user ID";
+    } else if (req.method === "POST" && !req.body.userId) {
+      // Si c'est un POST et userId n'existe pas, renvoyer un erreur
+      throw "User ID required";
+    } else if (req.method === "DELETE" || req.method === "PUT") {
+      // Si c'est un DELETE ou PUT, vérifier si le créateur de la sauce est bien le user (depuis token)
+      Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+        if (sauce.userId !== userId) {
+          res.status(401).json({
+            error: "Unauthorized request!",
+          });
+        } else {
+          next();
+        }
+      });
     } else {
       next();
     }
